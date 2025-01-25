95d757e4a068a7df1c4b01e3f5dae88ba03b1c73
Justin Moser <jmmoser15@gmail.com>
2025-01-25 10:16:53 -0600

    ack

--- src/test_server.zig
+++ src/test_server.zig
@@ -10,6 +10,24 @@ const utils = @import("utils.zig");
 
 const stdout = std.io.getStdOut().writer();
 
+fn encodeResponse(
+    allocator: std.mem.Allocator,
+    req: []const u8,
+    commandType: u16,
+) ![]u8 {
+    return encoding.encode(
+        allocator,
+        encoding.getHeaderTagged(req, 0),
+        encoding.getHeaderSource(req, 0),
+        TARGET,
+        false,
+        false,
+        encoding.getHeaderSequence(req, 0),
+        commandType,
+        null,
+    );
+}
+
 const TARGET: [6]u8 = [_]u8{ 0x97, 0x98, 0x99, 0x100, 0x101, 0x102 };
 pub fn main() !void {
     try network.init();
@@ -26,5 +44,27 @@ pub fn main() !void {
     var buffer: [1024]u8 = undefined;
     while (true) {
         const recv_result = try sock.receiveFrom(&buffer);
+        const slice = buffer[0..recv_result.numberOfBytes];
+        const responseFlags = encoding.getHeaderResponseFlags(slice);
+        const ackRequired = encoding.getHeaderAcknowledgeRequired(responseFlags);
+        if (ackRequired) {
+            const message = encodeResponse(allocator, slice, constants.CommandType.Acknowledgement) catch |err| {
+                std.debug.print("Failed to encode response: {any}\n", .{err});
+                continue;
+            };
+            defer allocator.free(message);
+            sock.sendTo(recv_result.sender, message) catch |err| {
+                std.debug.print("Failed to send message to {any}: {any}\n", .{ recv_result.sender, err });
+            };
+        }
+
+        switch (encoding.getHeaderType(slice, 0)) {
+            constants.CommandType.GetService => {
+                std.debug.print("Received GetService from {any}\n", .{recv_result.sender});
+            },
+            else => {
+                std.debug.print("Received unknown message from {any}\n", .{recv_result.sender});
+            },
+        }
     }
 }
