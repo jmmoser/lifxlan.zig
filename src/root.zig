fa55b23beb3919e336228165f7e215954822671e
Justin Moser <jmmoser15@gmail.com>
2025-02-02 16:09:33 -0600

    Removes commented code

--- src/root.zig
+++ src/root.zig
@@ -9,14 +9,6 @@ pub const commands = @import("commands.zig");
 pub const encoding = @import("encoding.zig");
 pub const constants = @import("constants.zig");
 pub const utils = @import("utils.zig");
-// pub const Client = @import("client.zig");
-// pub const commands = @import("commands.zig");
-// pub const constants = @import("constants.zig");
-// pub const devicesMod = @import("devices.zig");
-// pub const encoding = @import("encoding.zig");
-// pub const router = @import("router.zig");
-// pub const types = @import("types.zig");
-// pub const utils = @import("utils.zig");
 
 test {
     std.testing.refAllDeclsRecursive(@This());
